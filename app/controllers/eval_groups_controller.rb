9b150c57eebbb6fd502612e776727846fbd51e9d
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:12:09 +0000

    Only eval_group user can access show page

--- app/controllers/eval_groups_controller.rb
+++ app/controllers/eval_groups_controller.rb
@@ -1,5 +1,5 @@
 class EvalGroupsController < ApplicationController
-  before_action :current_user_must_be_eval_group_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_eval_group_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_eval_group_user
     eval_group = EvalGroup.find(params[:id])
