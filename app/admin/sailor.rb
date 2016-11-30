f83ea4775fcbad9b6ecaa52cdca7f5919b959c7a
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:00 +0000

    Whitelist columns in Sailor for ActiveAdmin

--- app/admin/sailor.rb
+++ app/admin/sailor.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Sailor do
 
+ permit_params :first_name, :last_name, :middle_name, :division, :command, :rank, :rating, :position, :collateral_duty_1, :collateral_duty_2, :collateral_duty_3, :primary_responsibility
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
