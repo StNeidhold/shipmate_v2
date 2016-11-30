72959a76c25d200c8147e0fee9fed87783fa231e
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:16 +0000

    Whitelist columns in Adjective for ActiveAdmin

--- app/admin/adjective.rb
+++ app/admin/adjective.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Adjective do
 
+ permit_params :adjective_word
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
