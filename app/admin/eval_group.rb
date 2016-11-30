944ce4d2616270d35dc946dc0dca8076bbfc09d7
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:04 +0000

    Whitelist columns in EvalGroup for ActiveAdmin

--- app/admin/eval_group.rb
+++ app/admin/eval_group.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register EvalGroup do
 
+ permit_params :group_name, :group_description, :group_rank, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
