1bc1b1b847399ef8b3886c87a896581ab1bb0bc6
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:43 +0000

    Whitelist columns in EvalAction for ActiveAdmin

--- app/admin/eval_action.rb
+++ app/admin/eval_action.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register EvalAction do
 
+ permit_params :action_description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
