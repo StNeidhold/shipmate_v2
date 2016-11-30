3af0693cb66a0ae173f8c458b3809b2d43ed8ebc
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:10:45 +0000

    Whitelist columns in BodyFormat for ActiveAdmin

--- app/admin/body_format.rb
+++ app/admin/body_format.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register BodyFormat do
 
+ permit_params :description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
