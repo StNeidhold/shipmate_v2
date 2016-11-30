6fd7ec10eeb7362e9dca446538051861482c4a15
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:56 +0000

    Whitelist columns in EvalEvidence for ActiveAdmin

--- app/admin/eval_evidence.rb
+++ app/admin/eval_evidence.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register EvalEvidence do
 
+ permit_params :evidence_description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
