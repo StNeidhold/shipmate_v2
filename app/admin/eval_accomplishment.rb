17d0a7d13184fe923d72a72d05a4e8e6b260c5a5
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:53 +0000

    Whitelist columns in EvalAccomplishment for ActiveAdmin

--- app/admin/eval_accomplishment.rb
+++ app/admin/eval_accomplishment.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register EvalAccomplishment do
 
+ permit_params :accomplishment_description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
