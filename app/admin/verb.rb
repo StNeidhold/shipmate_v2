10363b504e2b00193da6ecd45f7f03811cec7bcc
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:40 +0000

    Whitelist columns in Verb for ActiveAdmin

--- app/admin/verb.rb
+++ app/admin/verb.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Verb do
 
+ permit_params :verb_word
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
