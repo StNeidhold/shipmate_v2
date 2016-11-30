fd65dce61236859f220feac7ea1b1c5be978a09b
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:48 +0000

    Whitelist columns in EvalResult for ActiveAdmin

--- app/admin/eval_result.rb
+++ app/admin/eval_result.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register EvalResult do
 
+ permit_params :results_description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
