bd13960b77529eae72bd9b918b28a4bf701c28fd
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:11:13 +0000

    Whitelist columns in Adverb for ActiveAdmin

--- app/admin/adverb.rb
+++ app/admin/adverb.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Adverb do
 
+ permit_params :adverb_word
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
