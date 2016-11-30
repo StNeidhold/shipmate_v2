ee5192d9b1fe32523bb61507ff3f24bed0e61b79
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:09:45 +0000

    Generate users resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Routes for the User resource:
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+
   # Routes for the Collateral_duty resource:
   # CREATE
   get "/collateral_duties/new", :controller => "collateral_duties", :action => "new"
