24bac6189af056117ab269224937df30d8342b93
Raghu Betina <raghu@firstdraft.com>
2016-11-30 23:10:32 +0000

    Whitelist columns in WriteUp for ActiveAdmin

--- app/admin/write_up.rb
+++ app/admin/write_up.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WriteUp do
 
+ permit_params :breakout_statement_text, :heading_opening1, :heading_opening2, :breakout_statement_format_id, :heading_opening, :heading_opening_format_id, :heading_second_opening, :body_format_id, :bullet_one, :bullet_two, :bullet_format_id, :bullet_three, :bullet_4, :closing, :eval_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
